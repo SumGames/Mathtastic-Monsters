2cef16b4dc899c6db0da9ac173b1685964b54e76
MrMonocle88 <32154128+MrMonocle88@users.noreply.github.com>
2018-02-10 19:07:00 +1000

    First Version of Parallax Background Complete

---
+++ Mathtastic Monsters/Assets/ScrollingObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollingObject : MonoBehaviour
+{
+    private Rigidbody2D scrollImage;
+    public float scrollSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D attached to this GameObject.
+        scrollImage = GetComponent<Rigidbody2D>();
+        //Start the object moving.
+        scrollImage.velocity = new Vector2(scrollSpeed, 0);
+    }
+    void Update()
+    {
+
+    }
+}
